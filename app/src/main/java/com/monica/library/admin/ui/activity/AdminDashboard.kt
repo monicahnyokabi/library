7f4a173fb8e3483974100b093f57dd59609b0d12
monicahnyokabi <wambuinyokabimn@gmail.com>
2020-09-27 02:34:18 +0300

    Added Admin Navigation

--- app/src/main/java/com/monica/library/admin/ui/activity/AdminDashboard.kt
+++ app/src/main/java/com/monica/library/admin/ui/activity/AdminDashboard.kt
@@ -2,11 +2,46 @@ package com.monica.library.admin.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.monica.library.R
+import kotlinx.android.synthetic.main.activity_admin_dashboard.*
 
 class AdminDashboard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.AppTheme_NoActionBar)
         setContentView(R.layout.activity_admin_dashboard)
+        setSupportActionBar(toolbar)
+
+
+//        version_text.text = "v${BuildConfig.VERSION_NAME}"
+
+        setupNavigation()
     }
+
+    private fun setupNavigation() {
+        val navController = Navigation.findNavController(this, R.id.fragment_main)
+        // Update action bar to reflect navigation
+        NavigationUI.setupActionBarWithNavController(this, navController, drawer_layout)
+        NavigationUI.setupWithNavController(nav_view, navController)
+        navController.addOnDestinationChangedListener { _, destination, _ -> }
+    }
+
+    override fun onBackPressed() {
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+
+    override fun onSupportNavigateUp() =
+        NavigationUI.navigateUp(findNavController(R.id.fragment_main), drawer_layout)
+
 }
